85711a1e70fd9a42f101d866a0ab1c33df28dddb
vovan <vovantuan7702@gmail.com>
2026-02-27 22:38:24 +0700

    chore(bootstrap): initialize user and listing Spring Boot services
    Create user-service and listing-service modules.
    Add planned Spring Boot dependencies for upcoming development.

---
+++ listing-service/src/test/java/com/listingservice/ListingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
