699896af57b75ca091223d69ea9981d9a90cb51c
DangDuyy <duyprovn987@gmail.com>
2026-03-09 08:45:05 +0700

    base document for search service

--- search-service/src/main/java/com/searchservice/config/ElasticsearchConfig.java
+++ search-service/src/main/java/com/searchservice/config/ElasticsearchConfig.java
@@ -1,7 +1,7 @@
 package com.searchservice.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.searchservice.repository")
