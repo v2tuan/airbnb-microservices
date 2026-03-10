216e437619a35a29e534bb82af9ad15fb893fd96
DangDuyy <duyprovn987@gmail.com>
2026-03-10 20:09:31 +0700

    basic docker compose for elasticsearch

--- search-service/src/main/java/com/searchservice/document/PricingDocument.java
+++ search-service/src/main/java/com/searchservice/document/PricingDocument.java
@@ -1,11 +1,13 @@
 package com.searchservice.document;
 
+import lombok.Data;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
+@Data
 public class PricingDocument {
   @Field(type = FieldType.Float)
-  private String baseFree;
+  private String basePrice;
 
   @Field(type = FieldType.Keyword)
   private String currency;
