42715762cbc54d183f0a1184a7b820780f376f2b
vovan <vovantuan7702@gmail.com>
2026-03-10 19:24:01 +0700

    feat(gateway): configure JWT security and add user-service route

---
+++ api-gateway/src/main/java/com/apigateway/configuration/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.apigateway.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Value("${app.api-prefix}")
+    String prefix;
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers(prefix + "/users/auth/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth ->
+                        oauth.jwt(Customizer.withDefaults())
+                );
+
+        return http.build();
+    }
+}
