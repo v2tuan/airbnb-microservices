9248752305eda27a1443da88cdcbfde1fba83544
vovan <vovantuan7702@gmail.com>
2026-06-07 01:04:18 +0700

    chore: remove search service

--- search-service/src/main/java/com/searchservice/config/ElasticsearchConfig.java
+++
@@ -1,9 +0,0 @@
-package com.searchservice.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-
-@Configuration
-@EnableElasticsearchRepositories(basePackages = "com.searchservice.repository")
-public class ElasticsearchConfig {
-}
