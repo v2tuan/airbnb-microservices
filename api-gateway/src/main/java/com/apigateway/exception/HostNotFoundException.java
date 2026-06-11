dcffe6aed1a86b07b53dfe9bcfce7c12f4b7895c
DangDuyy <duyprovn987@gmail.com>
2026-06-12 00:33:54 +0700

    remove bff pattern

--- api-gateway/src/main/java/com/apigateway/exception/HostNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.apigateway.exception;
-
-public class HostNotFoundException extends RuntimeException {
-    public HostNotFoundException(String message) {
-        super(message);
-    }
-}
